5d38f2c985874071c62a5e76caa3e60369c93fd9
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-02-16 13:12:35 -0500

    more stuff

--- src/main/java/com/tfar/stellarfluidconduits/Main.java
+++ src/main/java/com/tfar/stellarfluidconduits/Main.java
@@ -1,14 +1,27 @@
 package com.tfar.stellarfluidconduits;
 
 import com.tfar.stellarfluidconduits.common.CommonProxy;
+import com.tfar.stellarfluidconduits.common.conduit.FluidConduitObject;
+import com.tfar.stellarfluidconduits.common.config.Config;
+import com.tfar.stellarfluidconduits.common.network.PacketHandler;
 import crazypants.enderio.api.addon.IEnderIOAddon;
+import crazypants.enderio.base.config.ConfigHandlerEIO;
+import crazypants.enderio.base.init.RegisterModObject;
 import info.loenwind.autoconfig.ConfigHandler;
+import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.Logger;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 @Mod.EventBusSubscriber(modid = ReferenceVariables.MOD_ID)
 
 @Mod(modid = ReferenceVariables.MOD_ID, name = ReferenceVariables.MOD_NAME, version = ReferenceVariables.VERSION)
@@ -21,13 +34,36 @@ public static CommonProxy proxy;
     private static ConfigHandler configHandler;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
+        configHandler = new ConfigHandlerEIO(event, Config.F);
+        proxy.preInit(event);
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
+        proxy.Init(event);
+        PacketHandler.init(event);
+    }
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        proxy.postInit(event);
+    }
+
+    @EventHandler
+    public void serverStart(FMLServerStartingEvent event) {
+        proxy.serverStart(event);
+    }
+
+    @SubscribeEvent
+    public static void registerBlocksEarly(@Nonnull RegisterModObject event) {
+        FluidConduitObject.registerBlocksEarly(event);
+    }
+
+    @Override
+    @Nullable
+    public Configuration getConfiguration() {
+        return Config.F.getConfig();
     }
 }
