943f23563b8b9d7c324b73d47218c28e975e2ea1
magcicada <l1334938030@gmail.com>
2020-06-30 09:06:32 +0800

    Update ReferenceVariables.java

--- src/main/java/com/tfar/stellarfluidconduits/ReferenceVariables.java
+++ src/main/java/com/tfar/stellarfluidconduits/ReferenceVariables.java
@@ -3,6 +3,6 @@ package com.tfar.stellarfluidconduits;
 public class ReferenceVariables {
     public static final String MOD_ID = "stellarfluidconduits";
     public static final String MOD_NAME = "Stellar Fluid Conduits";
-    public static final String VERSION = "1.12.2-1.0.0";
-    public static final String DEPENDENCIES = "required-after:enderioconduits@[5.0.50,);";
+    public static final String VERSION = "1.12.2-1.0.1";
+    public static final String DEPENDENCIES = "required-after:enderioconduits@[5.2.0,);";
 }
